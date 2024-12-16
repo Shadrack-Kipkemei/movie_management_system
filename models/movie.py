569f5e513243554fbebde957981343b66420f1be
sktanui <shadrackkemei2019@gmail.com>
2024-12-16 10:41:43 +0300

    Add Movie model with relationships to Director and Actor

---
+++ models/movie.py
@@ -0,0 +1,17 @@
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
+from models.base import Base
+
+
+# Movie model
+class Movie(Base):
+    __tablename__ = 'movies'
+
+    id = Column(Integer, primary_key=True) # Primary key
+    title = Column(String, nullable=False) # Movie title
+    release_year = Column(Integer) # Release year
+    director_id = Column(Integer, ForeignKey('directors.id')) # Foreign key to director
+
+    # Relationships
+    director = relationship('Director', back_populates = 'movies')
+    actors = relationship('Actor', secondary = 'movie_actors', back_populates = 'movies')
