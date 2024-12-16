50f96ea534ea4d673b8522e33a0e9446ab2359ef
sktanui <shadrackkemei2019@gmail.com>
2024-12-16 11:18:49 +0300

    Add association table for many-to-many relationship between Movies and Actors

---
+++ models/movie_actors.py
@@ -0,0 +1,9 @@
+from sqlalchemy import Table, Column, Integer, ForeignKey
+from models.base import Base
+
+
+# Association tabel between movies and actors
+movie_actors = Table('movie_actors', Base.metadata,
+                     Column('movie_id', Integer, ForeignKey('movies.id')),
+                     Column('actor_id', Integer, ForeignKey('actors.id'))
+)
